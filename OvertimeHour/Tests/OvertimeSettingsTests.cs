c25d7a4d2b18f5ebdb04189bb9bd7f544aa349d1
cash <cash@cashwu.com>
2023-06-15 21:27:26 +0800

    refactor

--- OvertimeHour/Tests/OvertimeSettingsTests.cs
+++ OvertimeHour/Tests/OvertimeSettingsTests.cs
@@ -9,98 +9,13 @@ public class OvertimeSettingsTests
 {
     /// <summary>
     /// setting
-    /// 06 - 22
-    ///
-    /// split setting
-    /// 06 - 22
-    /// </summary>
-    [Fact]
-    public void ctor_1_setting()
-    {
-        var baseDate = new DateTime(2023, 06, 01);
-
-        var overtimeSetting = new OvertimeSetting(new Period(baseDate, "06:00", "22:00"),
-                                                  new Rate(150));
-
-        var overtimeSettings = new OvertimeSettings(overtimeSetting);
-
-        overtimeSettings.Count.Should().Be(1);
-    }
-
-    /// <summary>
-    /// setting
-    /// 22 - 06
-    ///
-    /// split setting
-    /// 22 - 00, 22 - 06
-    /// </summary>
-    [Fact]
-    public void ctor_1_setting_cross_day()
-    {
-        var baseDate = new DateTime(2023, 06, 01);
-
-        var overtimeSetting = new OvertimeSetting(new Period(baseDate, "22:00", "06:00"),
-                                                  new Rate(200, 210));
-
-        var overtimeSettings = new OvertimeSettings(overtimeSetting);
-
-        overtimeSettings.Count.Should().Be(2);
-
-        var overtimeSettingFirst = overtimeSettings[0];
-        overtimeSettingFirst.Period.Start.Should().Be(new DateTime(2023, 06, 01, 22, 00, 00));
-        overtimeSettingFirst.Period.End.Should().Be(new DateTime(2023, 06, 02, 00, 00, 00));
-        overtimeSettingFirst.Rate.Should().BeEquivalentTo(new Rate(200, 210));
-
-        var overtimeSettingSecond = overtimeSettings[1];
-        overtimeSettingSecond.Period.Start.Should().Be(new DateTime(2023, 06, 02, 00, 00, 00));
-        overtimeSettingSecond.Period.End.Should().Be(new DateTime(2023, 06, 02, 06, 00, 00));
-        overtimeSettingSecond.Rate.Should().BeEquivalentTo(new Rate(200, 210));
-    }
-
-    /// <summary>
-    /// setting
-    /// 06 - 22, 22 - 06
-    ///
-    /// split setting
-    /// 06 - 22, 22 - 00, 00 - 06
-    /// </summary>
-    [Fact]
-    public void ctor_2_setting()
-    {
-        var baseDate = new DateTime(2023, 06, 01);
-
-        var daySetting = new OvertimeSetting(new Period(baseDate, "06:00", "22:00"),
-                                             new Rate(150));
-
-        var nightSetting = new OvertimeSetting(new Period(baseDate, "22:00", "06:00"),
-                                               new Rate(200, 210));
-
-        var overtimeSettings = new OvertimeSettings(daySetting, nightSetting);
-
-        overtimeSettings.Count.Should().Be(3);
-
-        var overtimeSettingFirst = overtimeSettings[0];
-        overtimeSettingFirst.Period.Start.Should().Be(new DateTime(2023, 06, 01, 06, 00, 00));
-        overtimeSettingFirst.Period.End.Should().Be(new DateTime(2023, 06, 01, 22, 00, 00));
-        overtimeSettingFirst.Rate.Should().BeEquivalentTo(new Rate(150));
-
-        var overtimeSettingSecond = overtimeSettings[1];
-        overtimeSettingSecond.Period.Start.Should().Be(new DateTime(2023, 06, 01, 22, 00, 00));
-        overtimeSettingSecond.Period.End.Should().Be(new DateTime(2023, 06, 02, 00, 00, 00));
-        overtimeSettingSecond.Rate.Should().BeEquivalentTo(new Rate(200, 210));
-
-        var overtimeSettingThird = overtimeSettings[2];
-        overtimeSettingThird.Period.Start.Should().Be(new DateTime(2023, 06, 02, 00, 00, 00));
-        overtimeSettingThird.Period.End.Should().Be(new DateTime(2023, 06, 02, 06, 00, 00));
-        overtimeSettingThird.Rate.Should().BeEquivalentTo(new Rate(200, 210));
-    }
-
-    /// <summary>
-    /// setting
     /// 06 - 22 (150), 22 - 06 (200, 210)
     ///
     /// split setting
     /// 06 - 22, 22 - 00, 00 - 06
+    ///
+    /// work
+    /// 08 - 18
     ///
     /// overtime
     /// 18 - 20
@@ -109,20 +24,13 @@ public class OvertimeSettingsTests
     /// 18 - 20 (150)
     /// </summary>
     [Fact]
-    public void split_period_have_1_overlap_not_cross_day()
+    public void day_overlap_not_cross_day()
     {
         var overtimeStart = new DateTime(2023, 06, 01, 18, 00, 00);
         var overtimeEnd = new DateTime(2023, 06, 01, 20, 00, 00);
         var overTimePeriod = new Period(overtimeStart, overtimeEnd);
 
-        var daySetting = new OvertimeSetting(new Period(overtimeStart.Date, "06:00", "22:00"),
-                                             new Rate(150));
-
-        var nightSetting = new OvertimeSetting(new Period(overtimeStart.Date, "22:00", "06:00"),
-                                               new Rate(200, 210));
-
-        var overtimeSettings = new OvertimeSettings(daySetting, nightSetting);
-
+        var overtimeSettings = GivenOvertimeSettings(overtimeStart.Date);
         var overtimes = overtimeSettings.CreateOvertime(overTimePeriod);
 
         overtimes.Should().BeEquivalentTo(new List<Overtime>
@@ -144,6 +52,47 @@ public class OvertimeSettingsTests
     /// split setting
     /// 06 - 22, 22 - 00, 00 - 06
     ///
+    /// work
+    /// 08 - 18
+    ///
+    /// overtime
+    /// 22 - 23
+    ///
+    /// split overtime
+    /// 22 - 23 (200)
+    /// </summary>
+    [Fact]
+    public void night_overlap_not_cross_day()
+    {
+        var overtimeStart = new DateTime(2023, 06, 01, 22, 00, 00);
+        var overtimeEnd = new DateTime(2023, 06, 01, 23, 00, 00);
+        var overTimePeriod = new Period(overtimeStart, overtimeEnd);
+
+        var overtimeSettings = GivenOvertimeSettings(overtimeStart.Date);
+        var overtimes = overtimeSettings.CreateOvertime(overTimePeriod);
+
+        overtimes.Should().BeEquivalentTo(new List<Overtime>
+        {
+            new()
+            {
+                Start = overtimeStart,
+                End = overtimeEnd,
+                Rate = 200,
+                Type = EnumRateType.Night
+            }
+        });
+    }
+
+    /// <summary>
+    /// setting
+    /// 06 - 22 (150), 22 - 06 (200, 210)
+    ///
+    /// split setting
+    /// 06 - 22, 22 - 00, 00 - 06
+    /// 
+    /// work
+    /// 08 - 18
+    ///
     /// overtime
     /// 22 - 01
     ///
@@ -151,19 +100,13 @@ public class OvertimeSettingsTests
     /// 22 - 00 (200), 00 - 01 (200)
     /// </summary>
     [Fact]
-    public void split_period_have_1_overlap_cross_day()
+    public void night_overlap_cross_day()
     {
         var overtimeStart = new DateTime(2023, 06, 01, 22, 00, 00);
         var overtimeEnd = new DateTime(2023, 06, 02, 01, 00, 00);
         var overTimePeriod = new Period(overtimeStart, overtimeEnd);
 
-        var daySetting = new OvertimeSetting(new Period(overtimeStart.Date, "06:00", "22:00"),
-                                             new Rate(150));
-
-        var nightSetting = new OvertimeSetting(new Period(overtimeStart.Date, "22:00", "06:00"),
-                                               new Rate(200, 210));
-
-        var overtimeSettings = new OvertimeSettings(daySetting, nightSetting);
+        var overtimeSettings = GivenOvertimeSettings(overtimeStart.Date);
 
         var overTimePeriods = overtimeSettings.CreateOvertime(overTimePeriod).ToList();
 
@@ -188,32 +131,28 @@ public class OvertimeSettingsTests
 
     /// <summary>
     /// setting
-    /// 06 - 17 (150), 17 - 06 (200, 210)
+    /// 06 - 22 (150), 22 - 06 (200, 210)
     ///
     /// split setting
-    /// 06 - 17, 17 - 00, 00 - 06
+    /// 06 - 22, 22 - 00, 00 - 06
+    /// 
+    /// work
+    /// 08 - 18
     ///
     /// overtime
-    /// 16 - 22
+    /// 20 - 23
     ///
     /// split overtime
-    /// 16 - 17 (150), 17 - 22 (210)
+    /// 20 - 22 (150), 22 - 23 (210)
     /// </summary>
     [Fact]
-    public void split_period_have_2_overlap_not_cross_day()
+    public void day_and_night_overlap_not_cross_day()
     {
-        var overtimeStart = new DateTime(2023, 06, 01, 16, 00, 00);
-        var overtimeEnd = new DateTime(2023, 06, 01, 22, 00, 00);
+        var overtimeStart = new DateTime(2023, 06, 01, 20, 00, 00);
+        var overtimeEnd = new DateTime(2023, 06, 01, 23, 00, 00);
         var overTimePeriod = new Period(overtimeStart, overtimeEnd);
 
-        var daySetting = new OvertimeSetting(new Period(overtimeStart, "06:00", "17:00"),
-                                             new Rate(150));
-
-        var nightSetting = new OvertimeSetting(new Period(overtimeStart, "17:00", "06:00"),
-                                               new Rate(200, 210));
-
-        var overtimeSettings = new OvertimeSettings(daySetting, nightSetting);
-
+        var overtimeSettings = GivenOvertimeSettings(overtimeStart.Date);
         var overTimePeriods = overtimeSettings.CreateOvertime(overTimePeriod);
 
         overTimePeriods.Should().BeEquivalentTo(new List<Overtime>
@@ -221,13 +160,13 @@ public class OvertimeSettingsTests
             new()
             {
                 Start = overtimeStart,
-                End = new DateTime(2023, 06, 01, 17, 00, 00),
+                End = new DateTime(2023, 06, 01, 22, 00, 00),
                 Rate = 150,
                 Type = EnumRateType.Day
             },
             new()
             {
-                Start = new DateTime(2023, 06, 01, 17, 00, 00),
+                Start = new DateTime(2023, 06, 01, 22, 00, 00),
                 End = overtimeEnd,
                 Rate = 210,
                 Type = EnumRateType.Night
@@ -237,32 +176,25 @@ public class OvertimeSettingsTests
 
     /// <summary>
     /// setting
-    /// 06 - 17 (150), 17 - 06 (200, 210)
+    /// 06 - 22 (150), 22 - 06 (200, 210)
     ///
     /// split setting
-    /// 06 - 17, 17 - 00, 00 - 06
+    /// 06 - 22, 22 - 00, 00 - 06
     ///
     /// overtime
-    /// 16 - 01
+    /// 20 - 01
     ///
     /// split overtime
-    /// 16 - 17 (150), 17 - 00 (210), 00 - 01 (210)
+    /// 20 - 22 (150), 22 - 00 (210), 00 - 01 (210)
     /// </summary>
     [Fact]
-    public void split_period_have_2_overlap_cross_day()
+    public void day_and_night_overlap_cross_day()
     {
-        var overtimeStart = new DateTime(2023, 06, 01, 16, 00, 00);
+        var overtimeStart = new DateTime(2023, 06, 01, 20, 00, 00);
         var overtimeEnd = new DateTime(2023, 06, 02, 01, 00, 00);
         var overTimePeriod = new Period(overtimeStart, overtimeEnd);
 
-        var daySetting = new OvertimeSetting(new Period(overtimeStart, "06:00", "17:00"),
-                                             new Rate(150));
-
-        var nightSetting = new OvertimeSetting(new Period(overtimeStart, "17:00", "06:00"),
-                                               new Rate(200, 210));
-
-        var overtimeSettings = new OvertimeSettings(daySetting, nightSetting);
-
+        var overtimeSettings = GivenOvertimeSettings(overtimeStart.Date);
         var overTimePeriods = overtimeSettings.CreateOvertime(overTimePeriod);
 
         overTimePeriods.Should().BeEquivalentTo(new List<Overtime>
@@ -270,13 +202,13 @@ public class OvertimeSettingsTests
             new()
             {
                 Start = overtimeStart,
-                End = new DateTime(2023, 06, 01, 17, 00, 00),
+                End = new DateTime(2023, 06, 01, 22, 00, 00),
                 Rate = 150,
                 Type = EnumRateType.Day
             },
             new()
             {
-                Start = new DateTime(2023, 06, 01, 17, 00, 00),
+                Start = new DateTime(2023, 06, 01, 22, 00, 00),
                 End = new DateTime(2023, 06, 02, 00, 00, 00),
                 Rate = 210,
                 Type = EnumRateType.Night
@@ -289,5 +221,16 @@ public class OvertimeSettingsTests
                 Type = EnumRateType.Night
             }
         });
+    }
+
+    private static OvertimeSettings GivenOvertimeSettings(DateTime baseDate)
+    {
+        var daySetting = new OvertimeSetting(new Period(baseDate, "06:00", "22:00"),
+                                             new Rate(150));
+
+        var nightSetting = new OvertimeSetting(new Period(baseDate, "22:00", "06:00"),
+                                               new Rate(200, 210));
+
+        return new OvertimeSettings(daySetting, nightSetting);
     }
 }
